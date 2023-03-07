e3f4271a991c2ac920e0840c24af4a4cc37a76a6
Mateus <mateus-botto@educar.rs.gov.br>
2023-03-07 15:22:10 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let favoritos = document.querySelector('.favoritos');
+let favoritado = document.querySelector('.marcado');
+
+let acao = document.querySelector('.acao');
+let terror = document.querySelector('.terror');
+let romance = document.querySelector('.romance');
+let drama = document.querySelector('.drama');
+
+let input = document.querySelector('.input');
+let procurar = document.querySelector('.procurar');
+
+let imagem = document.querySelector('.imagem');
+let nome = document.querySelector('.nome');
+
+let apifilmes = async 
+
+favoritos.addEventListener('click', =>{
+
+})
+
+acao.addEventListener('click', =>{
+
+})
+
+terror.addEventListener('click', =>{
+
+})
+
+romance.addEventListener('click', =>{
+
+})
+
+drama.addEventListener('click', =>{
+
+})
